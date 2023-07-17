63aa329d4471528bed2b28b1559682c582800676
Wesley Kauan de Sousa Silva <wesley.kauan@academico.ifrn.edu.br>
2023-07-17 17:41:23 -0300

    comitando

---
+++ aluno/forms.py
@@ -0,0 +1,36 @@
+from django.forms import ModelForm
+from django import forms
+from .models import Aluno, Curso, Cidade
+
+class AlunoForm(ModelForm):
+
+    class Meta:
+        model = Aluno
+        fields = '__all__'
+        widgets = {
+            'nome_aluno' : forms.TextInput(attrs={'class': 'form-control' }),
+            'endereco' : forms.TextInput(attrs={'class': 'form-control' }),
+            'email' : forms.EmailInput(attrs={'class': 'form-control' }),
+            'cidade': forms.Select(attrs={'class': 'form-control' }),
+            'curso': forms.Select(attrs={'class': 'form-control' })
+        }
+
+class CursoForm(ModelForm):
+
+    class Meta:
+        model = Curso
+        fields = '__all__'
+        widgets = {
+            'nome' : forms.TextInput(attrs={'class': 'form-control' })
+        }
+
+class CidadeForm(ModelForm):
+
+    class Meta:
+        model = Cidade
+        fields = '__all__'
+        widgets = {
+            'nome' : forms.TextInput(attrs={'class': 'form-control' }),
+            'sigla_estado' : forms.TextInput(attrs={'class': 'form-control' })
+
+        }
